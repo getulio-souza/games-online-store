5a9506fa204e77e1f62e0bc272d627da8c972149
Getulio Souza <getuliosouza8@gmail.com>
2024-12-11 16:30:36 -0300

    feature: structuring-html-header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'header', component: HeaderComponent},
+];
