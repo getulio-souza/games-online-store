042027e96b8de79e9d33610ec4e195da31f58c0b
Getulio Souza <getuliosouza8@gmail.com>
2024-12-12 16:28:31 -0300

    feature: working-on-add-produts-to-the-cart

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ import { ChangeDetectorRef } from '@angular/core';
 export class ProductListComponent implements OnInit {
   constructor(private cdr: ChangeDetectorRef) {}
 
+  disableBtn: boolean = false;
+  // name: string = '';
+  addToCart: number = 0;
+
   ngOnInit(): void {
     this.calculateDiscountedPrices();
   }
@@ -26,7 +30,10 @@ export class ProductListComponent implements OnInit {
       price: 400,
       discont: 0.1,
       itemWithDiscont: 0,
+      inStock: 10,
+      imageProduct: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Sony-PlayStation-4-PS4-wDualShock-4.jpg/1200px-Sony-PlayStation-4-PS4-wDualShock-4.jpg'
     },
+
   ];
 
   calculateDiscountedPrices() {
@@ -44,4 +51,23 @@ export class ProductListComponent implements OnInit {
     }
     return null;
   }
+
+  // onNameChange(event: any){
+  //   console.log(event.target.value)
+  //   this.name = event.target.value;
+  // }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue(){
+    //if the products i wanna add to the cart are less than the products i have in stock, i will be able to add more items to my cart
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+  }
+
 }
